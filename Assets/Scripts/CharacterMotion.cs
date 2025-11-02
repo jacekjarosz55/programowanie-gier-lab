fad86e7d42c5c5649a0a419fbcf1e625b8327976
Scarecat <scarecat@tuta.io>
2025-11-02 14:05:59 +0100

    player motion = jump, crouch

--- Assets/Scripts/CharacterMotion.cs
+++ Assets/Scripts/CharacterMotion.cs
@@ -1,10 +1,17 @@
 using System;
+using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+
 public class CharacterMotion : MonoBehaviour
 {
+
+    private bool isJumping = false;
+    private bool isCrouching = false;
+
     private CharacterController controller;
     private Transform camPivot;
 
@@ -14,33 +21,53 @@ public class CharacterMotion : MonoBehaviour
     public InputActionProperty crouchAction;
 
     public float sensitivity = 1.0f;
+    public float walkSpeed = 1.0f;
+    public float crouchSpeed = 0.5f;
 
 
-    public float jumpForce = 40.0f;
+    public float jumpForce = 20.0f;
+    public float jumpAttentuation = 10.0f;
     private float jumpVelocity = 0.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         controller = GetComponent<CharacterController>();
         jumpAction.action.performed += JumpAction_Performed;
-        crouchAction.action.performed += CrouchAction_Performed;
+        crouchAction.action.started += CrouchAction_Started;
+        crouchAction.action.canceled += CrouchAction_Cancelled;
         camPivot = GameObject.Find("Camera Pivot").transform;
     }
 
-    private void CrouchAction_Performed(InputAction.CallbackContext context)
+
+    private void CrouchAction_Started(InputAction.CallbackContext context)
     {
-        Debug.Log("crouch");
+        if(!isJumping && controller.isGrounded)
+        {
+            Debug.Log("crouch start!");
+            isCrouching = true;
+        }
     }
+
+    private void CrouchAction_Cancelled(InputAction.CallbackContext context)
+    {
+        if (isCrouching)
+        {
+            Debug.Log("crouch end!");
+            isCrouching = false;
+        }
+    }
+
 
     private void JumpAction_Performed(InputAction.CallbackContext obj)
     {
-        Debug.Log("jump");
-        if (controller.isGrounded)
+        if (controller.isGrounded && !isJumping && !isCrouching)
         {
+            Debug.Log("jumping!");
+            isJumping = true;
             jumpVelocity = jumpForce;
         }
     }
@@ -56,21 +83,33 @@ public class CharacterMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isJumping && controller.isGrounded && jumpVelocity == 0)
+        {
+            isJumping = false;
+        }
+
         Vector2 direction = movementAction.action.ReadValue<Vector2>();
         float gravityY = Physics.gravity.y;
 
-        float zMotion = direction.y * 1.0f;
-        float xMotion = direction.x * 1.0f;
+        float moveSpeed = isCrouching ? crouchSpeed : walkSpeed;
+
+        float zMotion = direction.y * moveSpeed;
+        float xMotion = direction.x * moveSpeed;
         float yMotion = jumpVelocity + gravityY;
 
-        controller.Move((transform.forward*zMotion + transform.right * xMotion + transform.up * yMotion) * Time.deltaTime);
+        Debug.Log($"isJumping = {isJumping}");
+        Debug.Log($"jumpVelocity = {jumpVelocity}");
+        Debug.Log($"gravityY = {gravityY}");
+        Debug.Log($"yMotion = {yMotion}");
+
+        controller.Move((transform.forward * zMotion + transform.right * xMotion + transform.up * yMotion) * Time.deltaTime);
+
+        jumpVelocity = Mathf.MoveTowards(jumpVelocity, 0, jumpAttentuation * Time.deltaTime);
+
+
         Vector2 look = lookAction.action.ReadValue<Vector2>();
         transform.Rotate(Vector3.up, look.x * sensitivity);
 
-        jumpVelocity = Mathf.MoveTowards(jumpVelocity, 0, Time.deltaTime);
-
-
-        
         Vector3 newEulerRotation = camPivot.eulerAngles + new Vector3(-look.y * sensitivity, 0f, 0f);
         newEulerRotation.x = ClampAngle(newEulerRotation.x, -90.0f, 90.0f);
         camPivot.eulerAngles = newEulerRotation;
