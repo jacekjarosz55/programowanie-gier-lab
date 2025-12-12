decccb3b61a6ad67e98d05728cb559ee9e9ab74d
Jacek Jarosz <jacekjarosz55@gmail.com>
2025-12-12 16:21:25 +0100

    Prevent null exception in ui

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,22 +9,17 @@ public class UIManager : MonoBehaviour
     public float MaxHealth { get => maxHealth; set { maxHealth = value; UpdateHealth(); } }
     public float CurrentHealth { get => currentHealth; set { currentHealth = value; UpdateHealth(); } }
 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void UpdateHealth()
-    {
-        healthBar.SetProgress(currentHealth/maxHealth);
-        Debug.Log($"Health updated: {currentHealth}/{maxHealth}");
-    }
-
     void Start()
     {
         healthBar = GameObject.Find("HealthBar").GetComponent<ProgressBar>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateHealth()
     {
-        
+        if (healthBar == null || maxHealth == 0) return;
+        healthBar.SetProgress(currentHealth/maxHealth);
+        Debug.Log($"Health updated: {currentHealth}/{maxHealth}");
     }
+
 }
